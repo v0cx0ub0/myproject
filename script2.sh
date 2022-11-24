e8fa9ed04b3c900015edbe31a3ec416af70bb409
v0cx0ub0 <galymzhan.ibadulla@gmail.com>
2022-11-23 22:38:47 -0800

    development

---
+++ script2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+for id in $(docker ps -q)
+do
+    if [[ $(docker port "${id}") == *"${1}"* ]]; then
+        echo "stopping container ${id}"
+        docker stop "${id}"
+        docker rm "${id}"
+    fi
+done
+
